aee72512cdc5c4a0e6d16026967bb782b0160cb7
David Diao <daviddiaoyf@gmail.com>
2017-07-05 15:07:35 +0800

    Added MainMenu Scene and Fonts for different languages

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	public void NewGame() {
+	}
+
+	public void LoadGame() {
+	}
+
+	public void Config() {
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
